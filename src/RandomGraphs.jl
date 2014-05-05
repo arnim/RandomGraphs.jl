5a29aeecfd3fd351f36a99ea1b9c2b23214a2859
Timothée Poisot <t.poisot@gmail.com>
2014-05-05 12:01:59 -0400

    Indentation fix

--- src/RandomGraphs.jl
+++ src/RandomGraphs.jl
@@ -1,18 +1,13 @@
 module RandomGraphs
-	
-	using Graphs
+   using Graphs
    using Distributions
-	
-	export
-	
-		barabasi_albert_graph
-		erdos_renyi_graph
-		watts_strogatz_graph
-		#=niche_model_graph=#
-
-		include("barabasi_albert.jl")
-		include("erdos_renyi.jl")
-		include("watts_strogatz.jl")
-		#=include("niche_model.jl")=#
-
+   export
+      barabasi_albert_graph
+      erdos_renyi_graph
+      watts_strogatz_graph
+      niche_model_graph
+   include("barabasi_albert.jl")
+   include("erdos_renyi.jl")
+   include("watts_strogatz.jl")
+   include("niche_model.jl")
 end
