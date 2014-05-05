a739cb9b3f3a7a7ba41471d71380761ed2cd8de6
Timothée Poisot <t.poisot@gmail.com>
2014-05-05 11:52:17 -0400

    Removed niche model / does it builds now?

--- src/RandomGraphs.jl
+++ src/RandomGraphs.jl
@@ -8,11 +8,11 @@ module RandomGraphs
 		barabasi_albert_graph
 		erdos_renyi_graph
 		watts_strogatz_graph
-		niche_model_graph
+		#=niche_model_graph=#
 
 		include("barabasi_albert.jl")
 		include("erdos_renyi.jl")
 		include("watts_strogatz.jl")
-		include("niche_model.jl")
+		#=include("niche_model.jl")=#
 
 end
