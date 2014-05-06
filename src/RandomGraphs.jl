e58d03b361d0b19220eb38481748de556bba0fed
Timothée Poisot <t.poisot@gmail.com>
2014-05-05 21:51:36 -0400

    Probably found the bug

--- src/RandomGraphs.jl
+++ src/RandomGraphs.jl
@@ -1,11 +1,13 @@
 module RandomGraphs
    using Graphs
    using Distributions
+
    export
-      barabasi_albert_graph
-      erdos_renyi_graph
-      watts_strogatz_graph
+      barabasi_albert_graph,
+      erdos_renyi_graph,
+      watts_strogatz_graph,
       niche_model_graph
+
    include("barabasi_albert.jl")
    include("erdos_renyi.jl")
    include("watts_strogatz.jl")
