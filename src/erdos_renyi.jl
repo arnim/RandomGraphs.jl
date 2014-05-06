12f61bff5cfdd97592d9246f3d77e3c776c10e0d
Timothée Poisot <t.poisot@gmail.com>
2014-05-06 19:23:04 -0400

    First attempt at a complete function for ER G(n,M)

--- src/erdos_renyi.jl
+++ src/erdos_renyi.jl
@@ -20,6 +20,33 @@ function erdos_renyi_graph(n::Integer, m::Real; is_directed=true, has_self_loops
    erdos_renyi_graph(g, n, m, has_self_loops=has_self_loops)
 end
 
+# Generate a random graph with n vertices and m edges.
+function erdos_renyi_graph{GT<:AbstractGraph}(g::GT, n::Integer, m::Integer; has_self_loops=false)
+   @assert(m > 0, "m must be larger than 0")
+   @assert(n > 1, "n must be larger than 1, currently $n")
+   nodepairs = Array((Integer, Integer), 0)
+   # TODO This is kinda lazy, we can compute the maximal number of edges
+   for i=1:n
+      start_ind = is_directed(g)? 1 : i
+      for j=start_ind:n
+         if (i != j || has_self_loops)
+            push!(nodepairs, (i, j))
+         end
+      end
+   end
+   shuffle!(nodepairs)
+   if m > length(nodepairs)
+      warn("You have picked a value of m larger than the maximal number of possible edges")
+      # TODO This should be done when calculating the maximal numbe of edges
+      m = length(nodepairs)
+   end
+   nodepairs = nodepairs[1:m]
+   for i in 1:m
+      add_edge!(g, nodepairs[i][1], nodepairs[i][2])
+   end
+   return g
+end
+
 # Convenience function for G(n,M)
 function erdos_renyi_graph(n::Integer, m::Integer; is_directed=true, has_self_loops=false)
    @printf "Erdos-Renyi G(n,M) model with %d nodes and M=%d" n m
--- test/erdos_renyi.jl
+++ test/erdos_renyi.jl
@@ -4,13 +4,19 @@ using Base.Test
 
 n = 100
 p = 0.5
+m = 30
 
-let u_er = erdos_renyi_graph(n, m, is_directed=false)
+let u_er = erdos_renyi_graph(n, p, is_directed=false)
 	@test num_vertices(u_er) == n
 	@test !is_directed(u_er)
 end
 
-let d_er = erdos_renyi_graph(n, m, is_directed=true)
+let d_er = erdos_renyi_graph(n, p, is_directed=true)
 	@test num_vertices(d_er) == n
 	@test is_directed(d_er)
+end
+
+let d_erm = erdos_renyi_graph(n, m, is_directed=true)
+	@test num_vertices(d_erm) == n
+	@test num_edges(d_erm) == m
 end
