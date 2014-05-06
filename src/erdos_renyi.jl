3b8626dee876da4083bc5f3a805cd19def5f2626
Timothée Poisot <t.poisot@gmail.com>
2014-05-05 22:11:11 -0400

    Made erdos_renyi more robust
    Added two assertions that the parameters passed are correct

--- src/erdos_renyi.jl
+++ src/erdos_renyi.jl
@@ -1,14 +1,16 @@
 # Generate a random graph with n vertices where each edge is included with probability p.
 function erdos_renyi_graph{GT<:AbstractGraph}(g::GT, n::Integer, p::Real; has_self_loops=false)
-    for i=1:n
-        start_ind = is_directed(g) ? 1 : i
-        for j=start_ind:n
-            if(rand() <= p && (i != j || has_self_loops))
-                add_edge!(g, i, j)
-            end
-        end
-    end
-    return g
+   @assert(p > 0 && p <= 1, "p must be in ]0;1], currently $p")
+   @assert(n > 1, "n must be larger than 1, currently $n")
+   for i=1:n
+      start_ind = is_directed(g) ? 1 : i
+      for j=start_ind:n
+         if(rand() <= p && (i != j || has_self_loops))
+            add_edge!(g, i, j)
+         end
+      end
+   end
+   return g
 end
 
 # Convenience function with a default graph type.
